478ab471a953be68d787afc24ec79226c511464c
Inès Varhol <inesvarhol@proton.me>
2023-12-29 18:21:25 +0100

    renaming html elements to 'box' and 'tag'

--- static/script.js
+++ static/script.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     // This function will run after the HTML document has been fully loaded
 
-    // Get the draggable box element
+    // Get the draggable tag element
     const draggables = document.querySelectorAll('.draggable');
 
-    draggables.forEach(function (draggableBox) {
+    draggables.forEach(function (draggableTag) {
 
         let offsetX, offsetY, isDragging = false;
         let anchorId = 4;
 
         // Event listener for mouse down event
-        draggableBox.addEventListener('mousedown', (e) => {
+        draggableTag.addEventListener('mousedown', (e) => {
             isDragging = true;
 
-            // Calculate the offset between the mouse position and the box position
-            offsetX = e.clientX - draggableBox.getBoundingClientRect().left - draggableBox.offsetWidth / 2;
-            offsetY = e.clientY - draggableBox.getBoundingClientRect().top - draggableBox.offsetHeight / 2;
+            // Calculate the offset between the mouse position and the tag position
+            offsetX = e.clientX - draggableTag.getBoundingClientRect().left - draggableTag.offsetWidth / 2;
+            offsetY = e.clientY - draggableTag.getBoundingClientRect().top - draggableTag.offsetHeight / 2;
 
             // Print the offset values to the console
             console.log('OffsetX:', offsetX);
@@ -25,19 +25,19 @@ document.addEventListener('DOMContentLoaded', function () {
         // Event listener for mouse move event
         document.addEventListener('mousemove', (e) => {
             if (isDragging) {
-                // Update the box position based on the mouse movement
-                draggableBox.style.left = e.clientX - offsetX + 'px';
-                draggableBox.style.top = e.clientY - offsetY + 'px';
+                // Update the tag position based on the mouse movement
+                draggableTag.style.left = e.clientX - offsetX + 'px';
+                draggableTag.style.top = e.clientY - offsetY + 'px';
 
-                // Print the updated box position to the console
-                console.log('Box Position - Left:', draggableBox.style.left, ', Top:', draggableBox.style.top);
+                // Print the updated tag position to the console
+                console.log('Tag Position - Left:', draggableTag.style.left, ', Top:', draggableTag.style.top);
             }
         });
 
         // Event listener for mouse up event
         document.addEventListener('mouseup', () => {
             isDragging = false;
-            snapToAnchor(draggableBox);
+            snapToAnchor(draggableTag);
 
             // Print a message when the dragging is stopped
             console.log('Dragging stopped');
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
             { x: 50, y: 70, id: 4 },
             { x: 80, y: 70, id: 5 }
         ];
-        function snapToAnchor(box) {
+        function snapToAnchor(tag) {
 
             // Calculate viewport dimensions
             const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
@@ -59,16 +59,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Convert percentage values to absolute coordinates
             const anchorPoints = anchorPointsPercentage.map(({ x, y, id }) => ({
-                x: (x / 100) * viewportWidth + box.offsetWidth / 2,
-                y: (y / 100) * viewportHeight + box.offsetHeight / 2,
+                x: (x / 100) * viewportWidth + tag.offsetWidth / 2,
+                y: (y / 100) * viewportHeight + tag.offsetHeight / 2,
                 id: id
             }));
 
             // Find the nearest anchor point
             const nearestAnchor = anchorPoints.reduce((nearest, anchor) => {
                 const distance = Math.hypot(
-                    box.offsetLeft + box.offsetWidth / 2 - anchor.x,
-                    box.offsetTop + box.offsetHeight / 2 - anchor.y
+                    tag.offsetLeft + tag.offsetWidth / 2 - anchor.x,
+                    tag.offsetTop + tag.offsetHeight / 2 - anchor.y
                 );
 
                 if (distance < nearest.distance) {
@@ -80,8 +80,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Snap to the nearest anchor point
             if (nearestAnchor.anchor) {
-                box.style.left = nearestAnchor.anchor.x - box.offsetWidth / 2 + 'px';
-                box.style.top = nearestAnchor.anchor.y - box.offsetHeight / 2 + 'px';
+                tag.style.left = nearestAnchor.anchor.x - tag.offsetWidth / 2 + 'px';
+                tag.style.top = nearestAnchor.anchor.y - tag.offsetHeight / 2 + 'px';
                 anchorId = nearestAnchor.anchor.id;
             }
 
@@ -90,11 +90,11 @@ document.addEventListener('DOMContentLoaded', function () {
                 const updatedWindowWidth = window.innerWidth || document.documentElement.clientWidth;
                 const updatedWindowHeight = window.innerHeight || document.documentElement.clientHeight;
                 console.log("resize to width ", updatedWindowWidth, " height ", updatedWindowHeight);
-                console.log("box at ", draggableBox.style.left, draggableBox.style.top);
+                console.log("tag at ", draggableTag.style.left, draggableTag.style.top);
                 console.log("anchor id is ", anchorId);
-                draggableBox.style.left = anchorPointsPercentage[anchorId].x / 100 * updatedWindowWidth + 'px';
-                draggableBox.style.top = anchorPointsPercentage[anchorId].y / 100 * updatedWindowHeight + 'px';
-                console.log("moved to ", draggableBox.style.left, draggableBox.style.top);
+                draggableTag.style.left = anchorPointsPercentage[anchorId].x / 100 * updatedWindowWidth + 'px';
+                draggableTag.style.top = anchorPointsPercentage[anchorId].y / 100 * updatedWindowHeight + 'px';
+                console.log("moved to ", draggableTag.style.left, draggableTag.style.top);
             });
         }
     });
